a389f803825ef5d766bd65375c0122e3c8402c15
zachoeh <zachoeh23@gmail.com>
2016-11-14 19:24:18 -0800

    documented radar class

--- src/Radar.java
+++ src/Radar.java
@@ -1,13 +1,28 @@
 /**
+ * This class is in charge of creating a Radar object which can be randomly spawned
+ * on a tile in the grid.
+ * 
  * Created by Ivan on 11/10/2016.
  */
 public class Radar implements Item, Cell{
+	
+	/**
+	 * The Radar constructor which allows the Radar item to be randomly spawned 
+	 * in the grid where there is a {@code Tile} object.
+	 */
     public Radar()
     {
     }
-
+    
+    /**
+     * This method returns a char value of {@code O} on the grid to represent 
+     * the location of the radar item.
+     * 
+     * @return O
+     */
     public char returnSymbol()
     {
         return 'O';
     }
 }
+
