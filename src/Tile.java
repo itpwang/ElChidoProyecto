2a8e5967c6334401d9598c3a88ed393bb02d9272
zachoeh <zachoeh23@gmail.com>
2016-11-14 19:25:16 -0800

    documented tile class

--- src/Tile.java
+++ src/Tile.java
@@ -1,7 +1,17 @@
 /**
+ * This class is in charge of returning the location of a tile not already occupied by 
+ * another object.
+ * 
  * Created by travis on 11/14/16.
  */
 public class Tile implements Cell{
+	
+	/**
+     * This method returns a char value of {@code /} on the grid to represent 
+     * the location of an unoccupied tile. 
+     * 
+	 * @return /
+	 */
     public char returnSymbol()
     {
        return '/';
