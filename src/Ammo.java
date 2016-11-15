a88587f17ee18aa578ef7e4751dc8c32cbdcd051
zachoeh <zachoeh23@gmail.com>
2016-11-14 19:17:27 -0800

    Update Ammo.java

--- src/Ammo.java
+++ src/Ammo.java
@@ -1,13 +1,25 @@
 /**
+ * This class is in charge of creating an Ammo object which can be randomly spawned
+ * on a tile in the grid.
+ * 
  * Created by Ivan on 11/10/2016.
  */
 public class Ammo implements Item, Cell{
 
-
+	/**
+	 * The ammo constructor which allows the ammo item to be randomly spawned in
+	 * the grid where there is a {@code Tile} object.
+	 */
     public Ammo()
     {
     }
 
+    /**
+     * This method returns a char value of {@code B} on the grid to represent 
+     * the location of the ammo item.
+     * 
+     * @return B
+     */
     public char returnSymbol()
     {
         return 'B';
