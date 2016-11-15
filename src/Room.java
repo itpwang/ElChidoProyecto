d22b596d6ad8c8c9f7c9b340971ffc33ab9ca55b
zachoeh <zachoeh23@gmail.com>
2016-11-14 19:26:06 -0800

    documented room class

--- src/Room.java
+++ src/Room.java
@@ -1,7 +1,16 @@
 /**
+ * This class is in charge of returning the location of each room on the grid.
+ * 
  * Created by travis on 11/14/16.
  */
 public class Room implements Cell {
+	
+	/**
+     * This method returns a char value of {@code R} on the grid to represent 
+     * the location of a room. 
+     * 
+	 * @return R
+	 */
     public char returnSymbol()
     {
         return 'R';
