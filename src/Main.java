8ab16632a2abbed2c4164f4452b00b8e7ce801d1
HiThereAndy <HiThereAndy@Andy>
2016-11-14 16:58:13 -0800

    Docced main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 /**
  * Created by Ivan on 11/10/2016.
  */
+/**
+ * This class represents the main class in our program. Program
+ * is launched here.
+ * @author HiThereAndy
+ *
+ */
 public class Main {
+	/*
+	 * This method creates a new UserInterface object, and calls to it's
+	 * constructor with the GameEngine parameter.
+	 */
     public static void main(String[] args) {
         UI ui= new UI(new GameEngine());
 
