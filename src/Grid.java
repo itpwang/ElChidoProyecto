fbf6fc1a78b067d08a8efe9a3a502b415c2c71ef
HiThereAndy <HiThereAndy@Andy>
2016-11-14 16:57:45 -0800

    Documented Main class as well as the Grid class

--- src/Grid.java
+++ src/Grid.java
@@ -1,15 +1,32 @@
 /**
  * Created by travis on 11/14/16.
  */
+/*
+ * This class represents the Grid object, utilized by the GameEngine
+ * to create and print the map the game will be played on.
+ */
 public class Grid {
+	/*
+	 * An object array of type {@link Cell} is created. The multidimensional
+	 * array allows for a 9 by 9 grid to be created.
+	 */
     public Cell[][] map = new Cell[9][9];
 
+    /*
+     * Constructor which is called to instantiate the map.
+     */
     Grid()
     {
-        for(int i = 0; i < map.length; i++)
+       /*
+        * This for loop allows each place in the multidimensional array to be instantiated as a Tile object.
+        */
+    	for(int i = 0; i < map.length; i++)
             for(int j = 0; j < map.length; j++)
                 map[i][j] = new Tile();
-
+    	/*
+    	 * Certain predisclosed tiles are set up as the Rooms in which the briefcase could potentially be
+    	 * located in.
+    	 */
         map[1][1] = new Room();
         map[4][1] = new Room();
         map[7][1] = new Room();
@@ -24,11 +41,19 @@ public class Grid {
         map[7][7] = new Room();
     }
 
+    /*
+     * This method returns a boolean value to determine if the tile the player wishes to be moved onto
+     * is empty. If true, the player may move onto it.
+     */
     private boolean isEmpty()
     {
         return false;
     }
 
+    /*
+     * This method is a void method which prints the map. Uses a for loop to run through the multi-dimensional
+     * array and prints out the subsequent tiles.
+     */
     public void printGrid()
     {
         for(int i = 0; i < map.length; i++){
