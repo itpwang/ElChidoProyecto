f081f594794b77af145e9c415c3c0e75d6cdf49c
Michael <michaelortega93@live.com>
2016-11-26 21:23:12 -0800

    Save Fully working, GameState class added with a test method, Quit after save implemented, many classes implementing serializable

---
+++ src/mainMod/GameState.java
@@ -0,0 +1,84 @@
+package mainMod;
+
+import java.awt.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class GameState implements Serializable {
+    private Point[] savedListOfEnemyLoc;
+    private Point[] savedListOfItemLoc;
+    private Boolean savedIsInvincible;
+    private Integer savedInvCounter;
+    private Integer savedPlayerAmmo;
+    private Boolean savedRadar;
+    private Enemy[] savedEnemies;
+    private Player savedPlayer;
+    private Grid savedBoard;
+
+    // ORDER :
+//                gameObjects.add(player);
+//                gameObjects.add(board);
+//                gameObjects.add(enemies);
+//                gameObjects.add(listOfEnemyLoc);
+//                gameObjects.add(listOfItemLoc);
+//                gameObjects.add(isInvincible);
+//                gameObjects.add(invCounter);
+//                gameObjects.add(playerAmmo);
+//                gameObjects.add(radar);
+    public GameState(ArrayList gameObjects) {
+        savedPlayer = (Player) gameObjects.get(0);
+        savedBoard = (Grid) gameObjects.get(1);
+        savedEnemies = (Enemy[]) gameObjects.get(2);
+        savedListOfEnemyLoc = (Point[]) gameObjects.get(3);
+        savedListOfItemLoc = (Point[]) gameObjects.get(4);
+        savedIsInvincible = (Boolean) gameObjects.get(5);
+        savedInvCounter = (Integer) gameObjects.get(6);
+        savedPlayerAmmo = (Integer) gameObjects.get(7);
+        savedRadar = (Boolean) gameObjects.get(8);
+
+
+    }
+
+
+    public void getSavedPlayer() {
+
+        System.out.println(savedPlayer.getNumOfLives()); //test
+
+    }
+
+//    public Point getSavedListOfEnemyLoc() {
+//        return savedListOfEnemyLoc;
+//    }
+//
+//    public Point getSavedListOfItemLoc() {
+//        return savedListOfItemLoc;
+//    }
+
+    public Boolean getSavedIsInvincible() {
+        return savedIsInvincible;
+    }
+
+    public Integer getSavedInvCounter() {
+        return savedInvCounter;
+    }
+
+    public Integer getSavedPlayerAmmo() {
+        return savedPlayerAmmo;
+    }
+
+    public Boolean getSavedRadar() {
+        return savedRadar;
+    }
+
+//    public Enemy getSavedEnemies() {
+//       // return savedEnemies;
+//    }
+
+    public Grid getSavedBoard() {
+        return savedBoard;
+    }
+
+}
+
+
